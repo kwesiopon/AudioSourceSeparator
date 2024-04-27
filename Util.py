62a63e73e5818fff2cc72552393bc571c23a21d7
Kwesi Opon <kwesiopon@outlook.com>
2024-04-27 13:45:16 -0500

    Added methods to convert audio numpy data to spectpgram and vice versa

--- Util.py
+++ Util.py
@@ -7,8 +7,11 @@ def resample(audio,orig_sr,new_sr):
     return librosa.resample(audio,orig_sr,new_sr)
 
 def load(path,sr):
+    #Outputs audio (n_frames,n_channels)
     y, sr = librosa.load(path,sr)
-    return y,sr
+    if len(y.shape) == 1:
+        y = np.expand_dims(y,axis=0)
+    return y.T,sr
 
 def getBatchFromDataSet(dataset_path,batch_size):
     #Get all audio file paths in the dataset path
@@ -24,6 +27,13 @@ def getBatchFromDataSet(dataset_path,batch_size):
     return batch_paths
 
 def getProcessedAudio(batch_path,target_sr):
+    '''
+    Converts all audio in the batch to numpy array format for analysis by the model
+
+    :param batch_path:
+    :param target_sr:
+    :return:
+    '''
     batch_data = []
     for file_path in batch_path:
         orig_sr = librosa.get_samplerate(file_path)
@@ -40,3 +50,18 @@ def getProcessedAudio(batch_path,target_sr):
 
     return batch_data
 
+def audio_numpy_to_spectogram(audio_data):
+    spectogram = librosa.stft( audio_data)
+    stft_magnitude, stft_phase = librosa.magphase(spectogram)
+
+    magnitude_dB = librosa.amplitude_to_db(stft_magnitude, ref=np.max)
+    return magnitude_dB, stft_phase
+
+
+def magnitude_db_phase_to_audio(magnitude_db,phase):
+    mag_reverse = librosa.db_to_amplitude(magnitude_db,ref=1.0)
+
+    audio_reverse_stft = mag_reverse * phase
+    reconstructed_audio = librosa.core.istft(audio_reverse_stft)
+
+    return reconstructed_audio
